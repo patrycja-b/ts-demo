dc02608ff76498ff8649bd4833c4db21eaf7f6b5
Patrycja Batko <patrycja.batko@netguru.co>
2020-03-09 20:56:28 +0100

    Add webpack

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from "./base-component.js";
-import { Autobind } from "../decorators/autobind.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { ProjectStatus, Project } from "../models/project.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectItem } from "./project-item.js";
+import { Component } from "./base-component";
+import { Autobind } from "../decorators/autobind";
+import { DragTarget } from "../models/drag-drop";
+import { ProjectStatus, Project } from "../models/project";
+import { projectState } from "../state/project-state";
+import { ProjectItem } from "./project-item";
 
 export class ProjectList extends Component<HTMLDivElement, HTMLElement>
   implements DragTarget {
--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,4 +1,4 @@
-import { ProjectStatus, Project } from "../models/project.js";
+import { ProjectStatus, Project } from "../models/project";
 
 // project state management
 type Listener<T> = (items: T[]) => void;
